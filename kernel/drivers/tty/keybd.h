29d8c3fd05bae2e75ace6fba48304b2753e42518
b0ric.alex <b0ric.alex@bc34febd-0b2b-ca3e-6f56-db577b75f5a2>
2010-08-16 22:51:44 +0000

    Keyboard layout modification

--- kernel/drivers/tty/keybd.h
+++ kernel/drivers/tty/keybd.h
@@ -27,12 +27,13 @@
  * A few words about how key_map[] works (in keymap.c) ans it's MACROs:
  * 1) we read 8-bit scancode from keyboard
  * 2) if it is break code we set break flag and convert it to make code
- * 3) thus i = value * KEYMAP_COLS + mod_index is in index in key_map[] array where
- *    mod_index - is a 'columns' selector to map the scancode if modifiers
+ * 3) thus i = value * KEYMAP_COLS + mod_index is in index in key_map[] array
+ *    where mod_index - is a 'columns' selector to map the scancode if modifiers
  *    (like SHIFT. ALT, CTRL, NUM, etc) were pressed
  * 4) real symbol is coded in low byte of keycode_t and modifiers are coded in
- *    high byte of keycode_t type (keycode_t is a word size). Here is how 
- *    modifiers are coded:
+ *    high byte of keycode_t type (keycode_t is a word size). But if the 
+ *    SPEC bit is set then low byte will represent special symbol code that are
+ *    listed below. Here is how modifiers are coded:
  *    |ALT|SHIFT|CTRL|SPEC|0|0|0|0|
  *      0    1    2    3   4 5 6 7
  *
@@ -40,23 +41,25 @@
  *    SHIFT  - SHIFT pressed
  *    CTRL   - CONTROL pressed
  *    SPEC   - SPECIAL key (F1, F2, ..., KeyPad(KP) keys, etc)
- *    NUM    - NUM LOCK is active
- *    CAPS   - CAPS LOCK is active
- *    SCROLL - SCROLL LOCK is active
  *
- * 5) such modifiers as NUM_LOCK, CAPS_LOCK and SCROLL_LOCK are processed
- *    explicitly in 'keybd.c'
+ * 5) other modifiers. that do not change character representation (like CTRL, 
+ *    NUM_LOCK, CAPS_LOCK and SCROLL_LOCK) are processed explicitly in 'keybd.c'
  */
 
 #define BREAK 0x80      // mask to check whether it is break or make code
 
 /* modifier bits */
+#define ALT     0x0100
+#define SHIFT   0x0200
+#define CTRL    0x0400
 #define SPECIAL 0x0800
 
 /* modifiers field macros */
-#define A(key)    (0x80 | (key))        // ALT (prints extended ASCII symbols)
-#define S(key)    (key)
-#define C(key)    (key)
+#define Alt(key)  (0x80  | (key))       // Alternate (prints extended ASCII symbols)
+#define A(key)    (ALT   | (key))       // ALT flag (for special keys)
+#define S(key)    (SHIFT | (key))       // SHIFT flag
+#define C(key)    (CTRL  | (key))       // CTRL flag
+#define AS(key)   (SHIFT | ALT | (key)) // ALT - SHIFT
 #define SP(key)   (SPECIAL | (key))     // SPECIAL key
 
 /* check for modifiers macros */
@@ -97,6 +100,19 @@
 #define KP8     0x38
 #define KP9     0x39
 #define KPDOT   0x3A
+#define INS     0x40
+#define DEL     0x41
+#define HOME    0x42
+#define END     0x43
+#define PGUP    0x44
+#define PGDOWN  0x45
+#define UP      0x46
+#define LEFT    0x47
+#define DOWN    0x48
+#define RIGHT   0x49
+#define LGUI    0x50    // Windows key
+#define RGUI    0x51    // Windown key
+#define APPS    0x52    // menu key
 
 /* types and prototypes */
 typedef uint8_t scancode_t;
