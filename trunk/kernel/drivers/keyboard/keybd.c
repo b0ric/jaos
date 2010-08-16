e511db841dbc0bf09b3aeb72f025c15af09041e8
b0ric.alex <b0ric.alex@bc34febd-0b2b-ca3e-6f56-db577b75f5a2>
2010-08-16 20:43:06 +0000

    Initial keyboard driiver. Keyboard layout

--- trunk/kernel/drivers/keyboard/keybd.c
+++ trunk/kernel/drivers/keyboard/keybd.c
@@ -24,13 +24,55 @@
 #define KBD_BUF_SZ 32           // software buffer fo keyboard scancodes
 
 static scancode_t kb_buffer[KBD_BUF_SZ] = {0};
-static uint8_t bufpos = 0;
+
+/* NumLock, ScrollLock and CapsLock status */
+static uint8_t locks;
+
+/* modifiers */
+static uint8_t lshift = 0, rshift = 0;
+static uint8_t lalt = 0, ralt = 0;
+static uint8_t lctrl = 0, rctrl = 0;
+
+/* misc */
+static uint8_t esc = 0;
 
 void keybd_handle_irq (void)
 {
-  scancode_t scode;
-  kb_buffer[bufpos % KBD_BUF_SZ] = inb (KBD_IO);
-  bufpos++;
+  scancode_t sc;
+  keycode_t kc;
+  
+  sc = inb (KBD_IO);
+  if (sc & BREAK) {
+        kc = keymap[sc & ~BREAK];
+        if (ISSPECIAL(kc))
+                kc &= ~SPECIAL;
+                /* we won't differ left and right modifiers */
+                switch (kc) {
+                        case LALT:
+                                lalt = 0;
+                                break;
+                        case RALT:
+                                ralt = 0;
+                                break;
+                        case LSHIFT:
+                                lshift = 0;
+                                break;
+                        case RSHIFT:
+                                rshift = 0;
+                                break;
+                        case LCTRL:
+                                lctrl = 0;
+                                break;
+                        case RCTRL:
+                                rctrl = 0;
+                                break;
+                        case default:
+                                break;
+                }
+  } else if (sc != ESC) {
+        kc = keymap[sc];
+  } else {
+        esc = 1;
+  }
 }
-
 
